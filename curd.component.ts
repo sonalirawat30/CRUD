5f5f4ad1b57ff35f48e0c86e7561e3b6291e2166
sonali30 <rawatsonali3003@gmail.com>
2020-12-21 23:53:17 -0700

    curd changes

---
+++ curd.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-curd',
+  templateUrl: './curd.component.html',
+  styleUrls: ['./curd.component.css']
+})
+export class CurdComponent implements OnInit {
+  data=[
+    {name:"Anju",Email:"abc@gmail.com"},
+    {name:"Ankit",Email:"xyz@gmail.com"},
+    {name:"Ajay",Email:"wxy@gmail.com"}
+  ];
+ 
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+    model:any={};
+    model1:any={};
+    addData(){
+      this.data.push(this.model);
+      this.model={};
+     
+    }
+    deleteData(i){
+      this.data.splice(i,1)
+  
+  }
+  myVal
+  editData(k)  {
+    this.model1.name=this.data[k].name;
+    this.model1.Email=this.data[k].Email;
+    this.myVal=k;
+  }
+
+}
